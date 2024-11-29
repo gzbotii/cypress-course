bcffd668995c912602a18ef8c436c079f8ba50f2
gzbotii <gzbotii@gmail.com>
2024-11-29 18:20:34 +0200

    Update fundamentals.cy.js

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,6 @@
 describe('Fundamentals test', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/fundamentals')
-    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing fun3damentals')
+    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
   })
 })
