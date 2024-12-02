5e3f6a91e048f1ddd5c6ebe83527c847ceb6ffc9
gzbotii <gzbotii@gmail.com>
2024-12-02 22:19:59 +0200

    form testing

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -2,5 +2,11 @@ describe('Forms test', () => {
     beforeEach(() => {
         cy.visit('/forms')
     })
-    it
+    it('Test subscribe form', () => {
+        cy.contains(/testing forms/i)
+        cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+        cy.get('@subscribe-input').type('boti@test.com')
+        cy.getDataTest('subscribe-button').click()
+        cy.contains(/Successfully subbed:/i).should('exist')
+    })
 })
