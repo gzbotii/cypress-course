9abfef4e5b273f7dc73d1cbaf453b5ca4541bb63
gzbotii <gzbotii@gmail.com>
2024-12-02 14:42:47 +0200

    Update fundamentals.cy.js

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,10 +1,13 @@
 describe('Fundamentals test', () => {
-  it('Contains correct header text', () => {
+  beforeEach(() => {
+
     cy.visit('/fundamentals')
+  })
+  it('Contains correct header text', () => {
     cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
   })
   it('Accordion test', () => {
-    cy.visit('/fundamentals')
+
     cy.contains('Your tests will exist in a describe block.').should('not.be.visible')
     cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
     cy.contains('Your tests will exist in a describe block.').should('be.visible')
