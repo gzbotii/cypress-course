d8fbec5c2bc4a5af4aeb446090f9615a8e5608af
gzbotii <gzbotii@gmail.com>
2024-12-03 14:04:45 +0200

    Getting to multipage

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,44 +1,28 @@
-import NavItem from "./NavItem"
+// NavBar.js
+import NavItem from "./NavItem";
 
 const navItems = [
-    {
-    label: 'Why Cypress?',
-    path: '/',
-    },
-    {
-    label: 'Overview',
-    path: '/overview'
-    },
-    {
-    label: 'Fundamentals',
-    path: '/fundamentals'
-    },
-    {
-        label: 'Forms',
-        path: '/forms'
-    },
-    {
-        label: 'Examples',
-        path: '/examples' 
-    },
-    {
-        label: 'Component',
-        path: '/component' 
-    },
-    {
-        label: 'Best Practices',
-        path: '/best-practices'
-    },
-]
+  {label: "Why Cypress?", path: "/"},
+  {label: "Overview", path: "/overview"},
+  {label: "Fundamentals", path: "/fundamentals"},
+  {label: "Forms", path: "/forms"},
+  {label: "Examples", path: "/examples"},
+].map((item) => ({
+  ...item,
+  dataTest: `nav-${item.label.toLowerCase().replace(/\s+/g, "-")}`,
+}));
 
-export default function NavBar(){
-    return (
-        <ul className="nav-bar">
-            {
-                navItems.map((item)=> (
-                    <NavItem key={item.label} label={item.label} path={item.path} />
-                ))
-            }
-        </ul>
-    )
+export default function NavBar() {
+  return (
+    <ul className="nav-bar">
+      {navItems.map((item) => (
+        <NavItem
+          key={item.label}
+          label={item.label}
+          path={item.path}
+          dataTest={item.dataTest} // Pass dataTest here
+        />
+      ))}
+    </ul>
+  );
 }
